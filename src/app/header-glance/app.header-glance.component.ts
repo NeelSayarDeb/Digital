ce618e1d12bb08c8a9dddf8c0b1e81e5eac646fd
BJW2K\ndeb <ndeb@bjs.com>
2018-12-18 05:55:31 -0500

    added bootstrap

--- src/app/header-glance/app.header-glance.component.ts
+++ src/app/header-glance/app.header-glance.component.ts
@@ -14,6 +14,46 @@ export class AppHeaderGlanceComponent {
         let d = new Date();
         this.date = d.getDate();
         this.monthValue = d.getMonth();
+        switch (this.monthValue) {
+            case 0:
+                this.month = "Jan"
+                break;
+            case 1:
+                this.month = "Feb"
+                break;
+            case 2:
+                this.month = "Mar"
+                break;
+            case 3:
+                this.month = "Apr"
+                break;
+            case 4:
+                this.month = "May"
+                break;
+            case 5:
+                this.month = "Jun"
+                break;
+            case 6:
+                this.month = "Jul"
+                break;
+            case 7:
+                this.month = "Aug"
+                break;
+            case 8:
+                this.month = "Sep"
+                break;
+            case 9:
+                this.month = "Oct"
+                break;
+            case 10:
+                this.month = "Nov"
+                break;
+            case 11:
+                this.month = "Dec"
+                break;
+            default:
+                break;
+        }
         this.year = d.getFullYear();
     }
 }
