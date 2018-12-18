c0178f7b3bac752369d3f85d2db476629142ad11
Kumari <skumari@bjs.com>
2018-12-18 08:46:28 -0500

    Calendar molecule added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppHeaderGlanceComponent } from './header-glance/app.header-glance.component';
+import { AppCalendarComponent } from './calendar/app-calendar.component';
+import { CalendarModule } from 'angular-calendar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppHeaderGlanceComponent
+    AppHeaderGlanceComponent,
+    AppCalendarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CalendarModule.forRoot(),
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
